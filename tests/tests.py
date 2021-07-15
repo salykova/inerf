7f38180454491a87c8cdb1ac7329a1dee49a1679
Aman <salykov4a@gmail.com>
2021-07-15 16:55:58 +0600

    delete torchsearchsorted, tests

--- tests/tests.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-from inerf_helpers import camera_transf
-import os
-import json
-
-with open(os.path.join("../data/nerf_synthetic/lego/obs_imgs", 'transforms.json'), 'r') as fp:
-    meta = json.load(fp)
-print(os.path.join("../data/nerf_synthetic/lego/obs_imgs", meta['frames'][0]['file_path'] + '.png'))
